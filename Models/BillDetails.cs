85341ff72a26cfa08e835f388c14f852b634c8ab
Pawan Kumar Vishwakarma <pawan07052001@gmail.com>
2024-08-11 15:07:29 +0530

    Status Update 11 August

---
+++ Models/BillDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BillGenerator.Models
+{
+    public class BillDetails
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public string MobileNumber { get; set; }
+        public string Address { get; set; }
+        public int TotalAmount { get; set; }
+
+    }
+}
