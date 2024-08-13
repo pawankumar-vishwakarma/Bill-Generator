9aa94bc6c31b7fc5bd42593167e46e49e7e08558
Pawan Kumar Vishwakarma <pawan07052001@gmail.com>
2024-08-13 19:43:51 +0530

    Models Created, status update 13 aug

--- Models/BillDetails.cs
+++ Models/BillDetails.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using BillGenerator.Models;
 
 namespace BillGenerator.Models
 {
@@ -12,6 +13,11 @@ namespace BillGenerator.Models
         public string MobileNumber { get; set; }
         public string Address { get; set; }
         public int TotalAmount { get; set; }
+        public List<Items> Items { get; set; }
 
+        public BillDetails()
+        {
+            Items = new List<Items>();
+        }
     }
 }
---
+++ Models/Items.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BillGenerator.Models
+{
+    public class Items
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+        public int ItemIndex { get; set; }
+    }
+}
