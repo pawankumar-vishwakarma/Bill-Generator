29fce5bbef09180894a457382e9a4059270b290d
Pawan Kumar Vishwakarma <pawan07052001@gmail.com>
2024-10-07 23:11:47 +0530

    Status Update

--- Repository/Data.cs
+++ Repository/Data.cs
@@ -113,6 +113,7 @@ namespace BillGenerator.Repository
             }
             finally
             {
+                //
                 // Close the connection in the finally block to ensure it's closed even in case of exceptions
                 if (con.State != System.Data.ConnectionState.Closed)
                 {
